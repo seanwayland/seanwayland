ab6e1edfe14d1fc5bfcc583ecd21300170096f65
seanwayland <seanwayland@gmail.com>
2019-07-07 14:13:28 -0400

    Add files via upload

---
+++ render.js
@@ -0,0 +1,57 @@
+"use strict";
+
+let xmlns = "http://www.w3.org/2000/svg"
+
+let board = []
+
+
+function emptyBoard() {
+    for (let x = 0; x < 12; x++) {
+        board[x] = 0
+    }
+
+}
+
+function toggleCell(x) {
+    board[x] = !board[x]
+}
+
+// determines whether a cell is alive
+function isAlive(x) {
+
+    return board[x];
+}
+
+
+function render() {
+
+    let gameDiv = document.getElementById("game")
+    gameDiv.innerHTML = ""
+
+
+    let row = document.createElementNS(xmlns, "g")
+    row.setAttribute("class", "gamerow")
+    gameDiv.appendChild(row)
+
+
+
+    let t = document.createElementNS(xmlns, "rect")
+    t.setAttribute("x", 23)
+    t.setAttribute("y", 0)
+    t.setAttribute("width", 23)
+    t.setAttribute("height", 120)
+    t.classList.add("whitekey")
+
+    if (isAlive(0)) {
+        t.classList.add("alive")
+    }
+
+    let handler = function(evt) {
+        ToggleCell(x)
+        render()
+    }
+
+    t.addEventListener("mousedown", handler)
+
+    row.appendChild(t)
+}
